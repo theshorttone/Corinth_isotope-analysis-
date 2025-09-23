dde11b9550007b883510158c6ae43944ecd25fd2
Zac Shortt <114637164+theshorttone@users.noreply.github.com>
2025-09-23 10:00:26 -0700

    updated clean soil and leaf

--- leaf_soil_combination.R
+++ leaf_soil_combination.R
@@ -11,9 +11,12 @@ library(tidyverse)
 
 leaf_data_filepath <- "data/corinth_leaf_2024.csv"
 soil_data_filepath <- "data/corinth_soil_2024.csv"
+dist_to_edge_filepath <- "data/corinth_dist_to_edge.csv"
+
 
 df_leaf <- read_csv(leaf_data_filepath)
 df_soil <- read_csv(soil_data_filepath)
+def_key <- read_csv(dist_to_edge_filepath)
 
 
 # data wrangling ----------------------------------------------------------
@@ -27,4 +30,29 @@ df_leaf_clean <- df_leaf %>%
     d13c = d13C,
     d15n = d15N
   ) %>% 
-  filter(!str_detect(sample_id, "peach|cocoa|blank"))
+  filter(!str_detect(sample_id, "peach|cocoa|blank")) %>% 
+  separate(sample_id, into = c("tree_number", "unit_plot", "location", "species"), sep = " ") %>%
+  separate(unit_plot, into = c("unit", "plot"), sep = "_")
+
+df_soil_clean <- df_soil %>% 
+  select(-((ncol(.)-7):ncol(.))) %>% 
+  rename(
+    g_id_number = `Sample ID`,
+    sample_weight = `Amount (mg)`,
+    percent_c = `%C`,
+    percent_n = `%N`,
+    d13c = d13C,
+    d15n = d15N,
+    comments = Comments
+  ) %>% 
+  filter(!str_detect(g_id_number, "AL")) %>% 
+  mutate(
+    id_number = str_remove(g_id_number, "^[0-9]+G_"),
+    duplicate = str_detect(id_number, "_DUP\\??$"),    
+    id_clean = str_remove(id_number, "_DUP\\??$")) %>% 
+  filter(is.na(comments) | !str_detect(comments, "Nitrogen QA/QC did not pass"))
+  
+  
+  
+
+
